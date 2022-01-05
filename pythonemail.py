6bf957097c0fe7908507bbf6a819fad7e3a0c8f3
Alexandre Damaso <alexandredamasocosta@gmail.com>
2022-01-04 23:59:34 -0300

    email python testado

--- pythonemail.py
+++ pythonemail.py
@@ -1,34 +1,61 @@
 import smtplib
-
 #SMTP - Simple Mail Transfer Protocol
 #Para criar o servidor e enviar o e-mail
-
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 #MIME - Norma de envio de mensagem pela internet
+from email.mime.base import MIMEBase
+from email import encoders
 
 # 01 - Start no servidor SMTP
-#host = 'smtp.gmail.com'
-#port = '587'
-#login = "faturamento.sesgo@gmail.com"
-#senha = "gapigapi"
+host = "smtp.gmail.com"
+port = "587"
+login = "cpi.sesgo@gmail.com"
+senha = "Cpi_32014991"
 
-server = smtplib.SMTP('smtp-relay.gmail.com', 587)
-#server.ehlo()
+server = smtplib.SMTP(host, port)
+server.ehlo()
 server.starttls()
-
-#server.login(login,senha)
-
-#server = smtplib.SMTP_SSL('smtp.gmail.com')
-#server.login(login, senha)
-#server.sendmail(
-#    "faturamento.sesgo@gmail.com",
-#    "cpi.sesgo@gmail.com",
-#    "mensagem de e-mail de teste"
-#)
-#server.quit()
-
+server.login(login,senha)
 
 # 02 - Construir o email timo MIME
+assunto = "Prévia da Síntese de Produção Ambulatorial - 12/2021"
+corpo = """
+<p>Prévia da Síntese de Produção Ambulatorial - 12/2021<br>
+Verificar ajustes e corrigir até 15/01/2022<br><br>
+Att.<br>Alexandre Damaso</p>
+"""
 
-# 03 - Enviar o email tipo MIME no servidor SMTP
+#Alexandre Damaso
+email_msg = MIMEMultipart()
+email_msg['From'] = login
+email_msg['To'] = "alexandredamasocosta@gmail.com"
+email_msg['Subject'] = assunto
+email_msg.attach(MIMEText(corpo,'html'))
+anexo = "D:\SFinal\R507-ALEXANDRE.TXT"
+attchment = open(anexo,'rb')
+att = MIMEBase('application', 'octet-stream')
+att.set_payload(attchment.read())
+encoders.encode_base64(att)
+att.add_header('Content-Disposition',f'attachment; filename=R507-ALEXANDRE.TXT')
+attchment.close()
+email_msg.attach(att)
+server.sendmail(email_msg['From'],email_msg['To'],email_msg.as_string())
+
+#Aliança Canecas
+email_msg = MIMEMultipart()
+email_msg['From'] = login
+email_msg['To'] = "aliancacanecas@gmail.com"
+email_msg['Subject'] = assunto
+email_msg.attach(MIMEText(corpo,'html'))
+anexo = "D:\SFinal\R507-CANECAS.TXT"
+attchment = open(anexo,'rb')
+att = MIMEBase('application', 'octet-stream')
+att.set_payload(attchment.read())
+encoders.encode_base64(att)
+att.add_header('Content-Disposition',f'attachment; filename=R507-CANECAS.TXT')
+attchment.close()
+email_msg.attach(att)
+server.sendmail(email_msg['From'],email_msg['To'],email_msg.as_string())
+
+server.quit()
